cb7559748623eb2fcc306fd434a35ff3e566d941
ilham_ath <ilham.athallah08@gmail.com>
2024-02-16 15:00:57 +0700

    new

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,7 +1,51 @@
 @extends('admin.parent')
 
 @section('content')
+    <div class="container">
+        <h1>CREATE USER</h1>
 
-    ini halaman create admin
-    
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">General Form Elements</h5>
+
+                    <!-- General Form Elements -->
+                    <form>
+                        <div class="row mb-3">
+                            <label for="inputText" class="col-sm-2 col-form-label">Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label class="col-sm-2 col-form-label">Select</label>
+                            <div class="col-sm-10">
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                        </div>
+
+                    </form><!-- End General Form Elements -->
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,5 +6,25 @@
     <a href="{{ route('admin.create') }}" class="btn btn-primary">create Admin</a>
 </div>
 
+{{-- table user --}}
+<div class="container text-center">
+    <h5 class="fw-bold">USER</h5>
+    <table class="table">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Action  </th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</div>
 
 @endsection
