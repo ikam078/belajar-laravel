9df346af2a087bac981a7d115b58f5e33d958cf2
ilham_ath <ilham.athallah08@gmail.com>
2024-02-20 14:09:59 +0700

    new

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,7 +1,21 @@
 @extends('admin.parent')
 
 @section('content')
-    
-    ini halaman create category
 
+<div class="card p-4">
+
+    <h1>Create Category</h1>
+
+    <form action="" method="post" enctype="multipart/form-data">
+
+        <div class="form-floating mb-3">
+            <input type="" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Name Category</label>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Create</button>
+
+    </form>
+
+</div>
 @endsection
