709e9f47228dbcbc75bcbb2970c82ed7a203183a
ilham_ath <ilham.athallah08@gmail.com>
2024-02-20 14:17:10 +0700

    new

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -6,10 +6,11 @@
 
     <h1>Create Category</h1>
 
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="{{ route('admin.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
 
         <div class="form-floating mb-3">
-            <input type="" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <input type="" class="form-control" id="floatingInput" placeholder="kesehatan">
             <label for="floatingInput">Name Category</label>
         </div>
 
