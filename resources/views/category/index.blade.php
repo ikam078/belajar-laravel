0d5ab49486d3c1dc43ad9dd1b517cc335cfe022b
ilham_ath <ilham.athallah08@gmail.com>
2024-02-21 14:48:35 +0700

    new

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -30,8 +30,8 @@
                     <td>{{ $row->iteration }}</td>
                     <td>{{ $row->name }}</td>
                     <td>
-                        <a href="{{ route('category.show', $row->id) }}" class="btn btn-info">Show Category</a>
-                        <form action="{{ route('category.destroy', $row-.id) }}" method="POST">
+                        <a href="" class="btn btn-info">Show Category</a>
+                        <form action="{{ route('category.show', $row->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
                         <button type="submit" class="btn btn-danger">
