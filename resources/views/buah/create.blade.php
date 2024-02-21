f6998ab8e991f484391056f6ee399e84fc95afd6
ilham_ath <ilham.athallah08@gmail.com>
2024-02-21 10:54:52 +0700

    new

---
+++ resources/views/buah/create.blade.php
@@ -0,0 +1,1 @@
+ini halaman buah create
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BuahController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,4 @@ Route::get('/', function () {
 
 Route::resource('admin', AdminController::class);
 Route::resource('category', CategoryController::class);
+Route::resource('buah', BuahController::class);
